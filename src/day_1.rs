e8a40d1e1487f6bc5c918056fe23790d1c68c0f3
Daniel Madrid <danimydev@gmail.com>
2023-12-02 19:00:25 -0500

    refactor day1.1

--- src/day_1.rs
+++ src/day_1.rs
@@ -10,35 +10,14 @@ pub fn problem_1() -> usize {
 }
 
 fn get_decoded_number(word: &str) -> usize {
-    let mut first = String::new();
-    let mut last = String::new();
-
-    for c in word.chars() {
-        if c.is_numeric() {
-            first.push(c);
-            break;
-        }
-    }
-
-    for c in word.chars().rev() {
-        if c.is_numeric() {
-            last.push(c);
-            break;
-        }
-    }
-
-    let decoded_number_str = format!("{}{}", first, last);
-    return decoded_number_str.parse().unwrap_or(0);
+    let digit_regex = Regex::new(r"\d").unwrap();
+    let matches: Vec<&str> = digit_regex.find_iter(&word).map(|m| m.as_str()).collect();
+    return format!("{}{}", matches[0], matches[matches.len() - 1]).parse().unwrap();
 }
 
 pub fn problem_2() -> usize {
     let mut result = 0;
     for line in read_to_string("./src/inputs/day_1.2.txt").unwrap().lines() {
-        println!(
-            "line: {} decoded: {}",
-            line,
-            get_decoded_number_with_words(line)
-        );
         result += get_decoded_number_with_words(line);
     }
     return result;
